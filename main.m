c2c22e7772e5a8b2071126522696bdf1dc1f31af
Vlad <vlad_pripotnev@hotmail.com>
2023-11-05 22:03:36 -0500

    added PPM calc and extra steps to main

--- main.m
+++ main.m
@@ -3,7 +3,8 @@ This is the main script for the MECH5103 project.
 This function will analyze a video and identify objects moving in it.
 %}
 
-clear all
+clear
+close all
 
 %%
 %video file location
@@ -13,6 +14,62 @@ imgRef = imread('/video/refImage.png');
 %turn video into a series of jpeg files
 [frameCount, imageDir] = videoProcessing(vFile);
 
+%Import world coordinate measurements and corresponding pixel points
+load("roadMeasurements.mat");
+
+%Select first frame for scene setup
+sceneImage = [imageDir, 'Frame1.jpg'];
+
+%Check if pixel coordinates have already been selected/loaded
+%Select pixel coordinates of matching world coordinate points
+if (exist("pixelMeasurements.mat","file") > 0)
+    fprintf("Pixel coordinates already saved, loading now.\n")
+    load("pixelMeasurements.mat");
+else
+    fprintf("No pixel coordinates known, please select from image.\n")
+    nPpmPoints = length(worldCoordinates_scene);
+    imageMatrixScene = imread(sceneImage,'jpg');
+    ppmFigNum = 7575;
+    figure(ppmFigNum)
+    imagesc(imageMatrixScene)
+    axis('equal')
+    [u1_ppm,v1_ppm] = ginput(nPpmPoints); 
+    close(ppmFigNum)
+end
+
+%Get PPM, pseudo inverse of PPM, and origin for pixel to world mapping
+pixelCoordinates_scene = [u1_ppm'; v1_ppm'];
+[PPM, PPMi, origin] = createPPM(pixelCoordinates_scene, ...
+    worldCoordinates_scene);
+
+%%
+%--------------------------------------------------------------------------
+%Example calculation, to be integrated into main loop when we have
+%centroid determination from pixel subtraction
+car_centroid_p = [1820, 681, 1]; %acquired from pixel coord (u,v,1)
+
+%calculate one of the solutions to the equation yielding a 1x4 vector
+%pixel coord = PPM * world coord
+%world coord = PPMi * pixel coord
+car_centroid_w = PPMi*car_centroid_p';
+
+%normalize for extra dimension
+car_centroid_w = car_centroid_w ./ car_centroid_w(4);
+
+%calculate unit vector of origin to test_p_w
+origin_to_point = car_centroid_w(1:3)-origin;
+origin_to_point_n = origin_to_point./norm(origin_to_point);
+
+%calculate intersection point
+plane_norm = [0;0;1];
+plane_loc = [0;0;0];
+
+%equation to get intersection of vector+point and plane norm+point
+intersect_p = origin + (plane_loc-origin)'*plane_norm/ ...
+    (plane_norm'*origin_to_point_n) * origin_to_point_n;
+%--------------------------------------------------------------------------
+
+%%
 %This is the main loop
 %read in each image one by one
 for i = 1:1:frameCount
--- roadPlane.m
+++ roadPlane.m
@@ -2,3 +2,4 @@
 %This exists as a place holder to test
 %eventually it will be the function to create the map of the road plane
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
