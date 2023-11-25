79165b72621b17edf892b3532a8a6f80c750004a
Vlad <vlad_pripotnev@hotmail.com>
2023-11-24 21:03:53 -0500

    initial velocity calculation commit

--- main.m
+++ main.m
@@ -14,16 +14,16 @@ vFile = ('/video/fourth1080.mp4');
 [frameCount, imageDir] = videoProcessing(vFile);
 
 %Import world coordinate measurements and corresponding pixel points
-load("roadMeasurements.mat");
+load("roadMeasurementsFinal.mat");
 
 %Select first frame for scene setup
-sceneImage = [imageDir, 'Frame1.jpg'];
+sceneImage = append(imageDir,'Frame1.jpg');
 
 %Check if pixel coordinates have already been selected/loaded
 %Select pixel coordinates of matching world coordinate points
-if (exist("pixelMeasurements.mat","file") > 0)
+if (exist("pixelMeasurementsFinal.mat","file") > 0)
     fprintf("Pixel coordinates already saved, loading now.\n")
-    load("pixelMeasurements.mat");
+    load("pixelMeasurementsFinal.mat");
 else
     fprintf("No pixel coordinates known, please select from image.\n")
     nPpmPoints = length(worldCoordinates_scene);
@@ -45,10 +45,63 @@ pixelCoordinates_scene = [u1_ppm'; v1_ppm'];
 %--------------------------------------------------------------------------
 %Example calculation, to be integrated into main loop when we have
 %centroid determination from pixel subtraction
-car_centroid_p = [1820, 681]; %acquired from pixel coord (u,v)
 
-[intersection,vect_n] = getWorldCoord(car_centroid_p,PPMi,origin);
+framerate = 60; %todo: use metadata framerate
 
+%example centroid data (-1 is invalid)
+centroids_u = [0 0 0 0 0 1 2 3 4 0 0 0 0 0 0 0;
+             0 1 2 3 4 5 0 0 0 0 0 0 0 0 0 0;
+             0 0 0 0 0 0 0 1 2 3 4 5 0 0 0 0;
+             0 0 0 0 0 0 0 0 0 0 0 0 1 2 3 4;
+             0 0 0 0 0 1 2 3 4 0 0 0 0 0 0 0;
+             0 0 0 0 0 1 2 3 4 0 0 0 0 0 0 0];
+centroids_v = [0 0 0 0 0 1 2 3 4 0 0 0 0 0 0 0;
+             0 1 2 3 4 5 0 0 0 0 0 0 0 0 0 0;
+             0 0 0 0 0 0 0 1 2 3 4 5 0 0 0 0;
+             0 0 0 0 0 0 0 0 0 0 0 0 1 2 3 4;
+             0 0 0 0 0 1 2 3 4 0 0 0 0 0 0 0;
+             0 0 0 0 0 1 2 3 4 0 0 0 0 0 0 0];
+maxCars = size(centroids_u,1);
+%first frame detected tracker
+centroidDetected = zeros(maxCars,1);
+%velocity per blob per frame (0 is invalid)
+velocitiesCars_x = zeros(maxCars,frameCount);
+velocitiesCars_y = zeros(maxCars,frameCount);
+
+%per frame
+for currFrame=1:frameCount
+    %go through every blob and see if it was detected in this frame
+    for carCnt=1:maxCars
+        car_centroid_p = [centroids_u(carCnt) centroids_v(carCnt)];
+        %centroid value is invalid
+        if (car_centroid_p(1) == -999)
+            %car has been processed and is now out of frame
+            %OR
+            %car has yet to enter frame
+            continue
+        %centroid value is real
+        else
+            %centroid yet to be initialized
+            if (centroidDetected(carCnt) == 0)
+                centroidDetected(carCnt) = currFrame;
+            end
+            %calculate intersection of this centroid
+            [intersection,vect_n] = getWorldCoord(car_centroid_p,PPMi,origin);
+        end
+        
+        %if centroid initialized in this frame, no velocity calc yet
+        if (centroidDetected(carCnt) == currFrame)
+            newCarPos = intersection;
+        %centroid initialized already, calculate velocity of past 2 frames
+        else
+            oldCarPos = newCarPos;
+            newCarPos = intersection;
+            velocity = getWorldVelocity(oldCarPos, newCarPos, framerate);
+            velocitiesCars_x(carCnt,currFrame) = velocity(1);
+            velocitiesCars_y(carCnt,currFrame) = velocity(2);
+        end
+    end
+end
 
 %%
 %Initialize rolling average for filter
