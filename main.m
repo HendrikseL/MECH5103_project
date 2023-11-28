93940d979759e4d664121bde5a226f3c479b99df
pauljcormier <pauljcormier@L-ME3290-72.ads.mae.carleton.ca>
2023-11-27 20:34:40 -0500

    Broke out blob analysis into separate function

--- main.m
+++ main.m
@@ -13,12 +13,12 @@ imgRef = imread('./video/Frame1.jpg');
 %turn video into a series of jpeg files
 [frameCount, imageDir] = videoProcessing(vFile);
 
-hBlob = vision.BlobAnalysis('AreaOutputPort',false,'BoundingBoxOutputPort',false);
+
 
 %This is the main loop
 %read in each image one by one
-centroids_u = ones([20, frameCount]).*-1;
-centroids_v = ones([20, frameCount]).*-1;
+centroids_u = ones([100, frameCount]).*-1;
+centroids_v = ones([100, frameCount]).*-1;
 for i = 1:1:frameCount
     %create image filepath
     imagePath = fullfile(imageDir, ['Frame' int2str(i), '.jpg']);
@@ -28,19 +28,13 @@ for i = 1:1:frameCount
     % Background subtraction with reference image
     diffImage = double(imgRef) - double(im);
     % and threshold above the noise level (say it's 10 gray levels.
-    mask = abs(diffImage) > 32;
+    mask = abs(diffImage) > 40;
     %display the white blob
     %store the file for later use
     mask = any(mask, 3);
 
     %Noise reduction and blob centroid detection
-    %TODO: change to bottom right corner of bounding box
-    mask = medfilt2(mask);
-    centroid = hBlob(mask);
-    for j = 1:1:size(centroid,1)
-        centroids_u(j,i) = centroid(j,1);
-        centroids_v(j,i) = centroid(j,2);
-    end
+    [centroids_u(:,i), centroids_v(:,i)] = blobAnalysis(mask);
     imshow(mask)
     hold on
     plot(centroids_u(:,i),centroids_v(:,i), 'r.');
